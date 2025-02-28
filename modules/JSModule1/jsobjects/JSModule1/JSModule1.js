c687d264b03f77163c7676a8b61e2fca21f8d574
kamakshi <kamakshi@appsmith.com>
2025-02-28 14:28:22 +0000

    First Commit

--- modules/JSModule1/jsobjects/JSModule1/JSModule1.js
+++ modules/JSModule1/jsobjects/JSModule1/JSModule1.js
@@ -1,13 +1,13 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	myFun1 (limit=1) {
+	myFun1 (limit=12) {
 		//	write code here
 		//	this.myVar1 = [1,2,3]
 		return QueryModule1.run({limit})
 	},
 	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
+			//	use async-await or promises
+			//	await storeValue('varName', 'hello world')
 	}
 }
